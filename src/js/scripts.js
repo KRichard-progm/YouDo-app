c71362fc156404edd5afdcb5be47861aa0ffd673
KRichard-progm <76790600+KRichard-progm@users.noreply.github.com>
2021-02-06 16:33:46 +0100

    MZ-4

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,3 @@
-
-
 function newElement() {
 
     let li = document.createElement("li");
@@ -7,24 +5,26 @@ function newElement() {
     let t = document.createTextNode(inputValue);
     li.appendChild(t);
     if (inputValue === '') {
-        alert("You must write something!");
-    } else {
-        document.getElementById("myUL").appendChild(li);
-    }
-    document.getElementById("myInput").value = "";
+        document.getElementsByClassName("addBtn").style.display = "block";
 
-    let span = document.createElement("button");
-    let txt = document.createTextNode("Delete");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
+    } else
+{
+    document.getElementById("myUL").appendChild(li);
+}
+document.getElementById("myInput").value = "";
 
-    for ( i = 0; i < close.length; i++) {
-        close[i].onclick = function () {
-            let item = this.parentElement;
-            item.style.display = "none";
-        }
+let span = document.createElement("button");
+let txt = document.createTextNode("Delete");
+span.className = "close";
+span.appendChild(txt);
+li.appendChild(span);
+
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        let item = this.parentElement;
+        item.style.display = "none";
     }
+}
 }
 
 let myYouDoList = document.getElementsByTagName("li");
@@ -40,7 +40,7 @@ for (i = 0; i < myYouDoList.length; i++) {
 
 let close = document.getElementsByClassName("close");
 let x;
-for (x= 0; x < close.length; x++) {
+for (x = 0; x < close.length; x++) {
     close[i].onclick = function () {
         const div = this.parentElement;
         div.style.display = "none";
@@ -49,39 +49,44 @@ for (x= 0; x < close.length; x++) {
 
 function checkChar() {
     let myChar = document.getElementById('myInput');
-
     let goodColor = "#66cc66";
     let badColor = "#ff6666";
 
     if (myChar.value.length >= 6) {
         myChar.style.backgroundColor = goodColor;
-        document.getElementsByClassName("addBtnBtn").style.display = "none";
+
     } else {
         myChar.style.backgroundColor = badColor;
-        document.getElementsByClassName("addBtnBtn").style.display = "block";
+        document.getElementsByClassName("addBtn").style.display = "block";
     }
 }
 
-function saveAll() {
 
-    const myArrStr = JSON.stringify(myArr);
-    localStorage.setItem("myInput", str);
+//The create button is disabled so far the input doesn't pass the validation.
 
-    function getAll() {
-        let str = localStorage.getItem("myInput");
-        myArrSrt = JSON.parse(str);
-        if (!myInput) {
-            myInput = [];
 
+//If we refresh the browser, the list remains.
+    function saveAll() {
+
+        const myArrStr = JSON.stringify(myArr);
+        localStorage.setItem("myInput", str);
+
+        function getAll() {
+            let str = localStorage.getItem("myInput");
+            myArrSrt = JSON.parse(str);
+            if (!myInput) {
+                myInput = [];
+
+            }
+        }
+
+        getAll();
+    }
+
+    function getExtension() {
+        let now = new Date();
+        let deadline = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() + 7));
+        let newTask = {
+            due: Utilities.formatDate(deadline, "GMT", "yyyy-MM-dd'T'HH:mm:ss'Z'")
         }
     }
-
-    getAll();
-}
-function getExtension(listId, taskId) {
-    let now = new Date();
-    let deadline = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() + 7));
-    let newTask = {
-        due: Utilities.formatDate(deadline, "GMT", "yyyy-MM-dd'T'HH:mm:ss'Z'")
-    }
-}
